1fbe03a849c06d35ff636a9b5fe63e4dd10b1c9f
Cory Spitzer <coryspitzer@gmail.com>
2016-02-22 10:22:42 -0500

    Give me a special greeting

--- hello.pl
+++ hello.pl
@@ -6,4 +6,9 @@ use warnings;
 print "What is your name? ";
 my $name = <STDIN>;
 chomp $name;
-print "Hello, $name!\n";
+my $lowercase_name = lc $name;
+if ($lowercase_name eq "cory") {
+  print "Greetings dear Cory!\n"
+} else {
+  print "Hello, $name!\n";
+}
